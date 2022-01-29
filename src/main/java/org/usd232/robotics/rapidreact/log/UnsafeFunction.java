33c64300912624418484abaa2f570a11bfbcda78
Noah Johnson <johnsnoa001@usd232.org>
2022-01-29 15:16:09 -0600

    Initial commit

--- src/main/java/org/usd232/robotics/rapidreact/log/UnsafeFunction.java
+++
@@ -1,6 +0,0 @@
-package org.usd232.robotics.rapidreact.log;
-
-@FunctionalInterface
-public interface UnsafeFunction<T> {
-    T run() throws Throwable;
-}
--- src/main/java/org/usd232/robotics/rapidreact/log/UnsafeRunnable.java
+++
@@ -1,6 +0,0 @@
-package org.usd232.robotics.rapidreact.log;
-
-@FunctionalInterface
-public interface UnsafeRunnable {
-    void run() throws Throwable;
-}
