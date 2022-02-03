c6f174512fae347c3e0eb32918715a72b117e5d7
Jack_W() <willijoh005@usd232.org>
2022-02-02 20:13:52 -0600

    Cleaned code / docs

--- src/main/java/org/usd232/robotics/rapidreact/Constants.java
+++ src/main/java/org/usd232/robotics/rapidreact/Constants.java
@@ -2,6 +2,9 @@ package org.usd232.robotics.rapidreact;
 
 import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,37 +16,52 @@ import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
 public final class Constants {
     public static final class DriveConstants {
         /**
-         * The left-to-right distance between the drivetrain wheels
+         * The left-to-right distance between the drive wheels
          *
          * Should be measured from center to center.
          */
         public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.5;
         /**
-         * The front-to-back distance between the drivetrain wheels.
+         * The front-to-back distance between the drive wheels.
          *
          * Should be measured from center to center.
         */
         public static final double DRIVETRAIN_WHEELBASE_METERS = 0.64928;
 
+        // Front left swerve module
         public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
         public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2;
         public static final int FRONT_LEFT_MODULE_STEER_CANCODER = 10;
         public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(329.15);
         
+        // Front right swerve module
         public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3;
         public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4;
         public static final int FRONT_RIGHT_MODULE_STEER_CANCODER = 9;
         public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(9.76);
         
+        // Back left swerve module
         public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 7;
         public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8;
         public static final int BACK_LEFT_MODULE_STEER_CANCODER = 12;
         public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(221.57);
         
+        // Back right swerve module
         public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 5;
         public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 6;
         public static final int BACK_RIGHT_MODULE_STEER_CANCODER = 11;
         public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(91.58);
+
+        public static final SwerveDriveKinematics DRIVE_KINEMATICS = new SwerveDriveKinematics(
+          // Front left
+          new Translation2d(DriveConstants.DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DriveConstants.DRIVETRAIN_WHEELBASE_METERS / 2.0),
+          // Front right
+          new Translation2d(DriveConstants.DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DriveConstants.DRIVETRAIN_WHEELBASE_METERS / 2.0),
+          // Back left
+          new Translation2d(-DriveConstants.DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DriveConstants.DRIVETRAIN_WHEELBASE_METERS / 2.0),
+          // Back right
+          new Translation2d(-DriveConstants.DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DriveConstants.DRIVETRAIN_WHEELBASE_METERS / 2.0)
+  );
     }
     
     public static final class ModuleConstants {
@@ -53,9 +71,9 @@ public final class Constants {
          * This is a measure of how fast the robot should be able to drive in a straight line.
          */
         // Freespeed: 4.96824 m/s^2
-        // Calculated: 4.4231268 m/s^2
+        // Calculated: 4.968230455 m/s^2
         // Real: TODO m/^2 (after whole robot is assembled)
-        public static final double MAX_VELOCITY_METERS_PER_SECOND = 5680.0 / 60.0 *
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 *
         SdsModuleConfigurations.MK4_L2.getDriveReduction() *
         SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI;
 
@@ -73,7 +91,7 @@ public final class Constants {
          */
         // Here we calculate the theoretical maximum angular velocity. You can also replace this with a measured amount.
         // Freespeed: 61.21537703 rad/s^2
-        // Calculated: 54.49885165 rad/s^2
+        // Calculated: 61.2152594258 rad/s^2
         // Real: TODO rad/s^2 (after whole robot is assembled)
         public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
         Math.hypot(DriveConstants.DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DriveConstants.DRIVETRAIN_WHEELBASE_METERS / 2.0); // hypot is about 0.08116 meters
--- src/main/java/org/usd232/robotics/rapidreact/commands/SwerveDrive.java
+++ src/main/java/org/usd232/robotics/rapidreact/commands/SwerveDrive.java
@@ -2,44 +2,64 @@ package org.usd232.robotics.rapidreact.commands;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import org.usd232.robotics.rapidreact.log.Logger;
 import org.usd232.robotics.rapidreact.subsystems.DriveSubsystem;
 
 import java.util.function.DoubleSupplier;
 
 public class SwerveDrive extends CommandBase {
-    private final DriveSubsystem m_drivetrainSubsystem;
+    /**
+     * The logger.
+     * 
+     * @since 2018
+     */
+    //@SuppressWarnings("unused")
+    private static final Logger LOG = new Logger();
+    
+    private final DriveSubsystem m_driveSubsystem;
 
     private final DoubleSupplier m_translationXSupplier;
     private final DoubleSupplier m_translationYSupplier;
     private final DoubleSupplier m_rotationSupplier;
 
-    public SwerveDrive(DriveSubsystem drivetrainSubsystem,
+    /** Makes the robot drive. To use, do SwerveDrive([x-speed], [y-speed], [rotation-in-degrees]) */
+    public SwerveDrive(DriveSubsystem driveSubsystem,
                                DoubleSupplier translationXSupplier,
                                DoubleSupplier translationYSupplier,
                                DoubleSupplier rotationSupplier) {
-        this.m_drivetrainSubsystem = drivetrainSubsystem;
+        this.m_driveSubsystem = driveSubsystem;
         this.m_translationXSupplier = translationXSupplier;
         this.m_translationYSupplier = translationYSupplier;
         this.m_rotationSupplier = rotationSupplier;
 
-        addRequirements(drivetrainSubsystem);
+        addRequirements(driveSubsystem);
     }
 
+    /** Sets ChassisSpeeds to the x speed, y speed, and rotation */
     @Override
     public void execute() {
         // You can use `new ChassisSpeeds(...)` for robot-oriented movement instead of field-oriented movement
-        m_drivetrainSubsystem.drive(
+        m_driveSubsystem.drive(
                 ChassisSpeeds.fromFieldRelativeSpeeds(
                         m_translationXSupplier.getAsDouble(),
                         m_translationYSupplier.getAsDouble(),
                         m_rotationSupplier.getAsDouble(),
-                        m_drivetrainSubsystem.getGyroscopeRotation()
+                        m_driveSubsystem.getGyroscopeRotation()
                 )
         );
     }
 
+    /** Checks if the conditions inside isFinished are true */
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+    /** If interrupted is true, then stop the robot from moving */
     @Override
     public void end(boolean interrupted) {
-        m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
+        LOG.info("Swerve Drive Command ended");
+        m_driveSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
     }
 }
