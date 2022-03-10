af92f71569aad92d0d46a542fef59f00daac0f5b
Jack_W() <willijoh005@usd232.org>
2022-03-10 17:18:37 -0600

    simplified (IDK why i didnt start here)

--- src/main/java/org/usd232/robotics/rapidreact/commands/Autonomous/Paths/OffLine.java
+++ src/main/java/org/usd232/robotics/rapidreact/commands/Autonomous/Paths/OffLine.java
@@ -1,7 +1,5 @@
 package org.usd232.robotics.rapidreact.commands.autonomous.paths;
 
-import org.usd232.robotics.rapidreact.commands.autonomous.AugerOn;
-import org.usd232.robotics.rapidreact.commands.autonomous.ShooterOn;
 import org.usd232.robotics.rapidreact.subsystems.AugerSubsystem;
 import org.usd232.robotics.rapidreact.subsystems.DriveSubsystem;
 import org.usd232.robotics.rapidreact.subsystems.ShooterSubsystem;
@@ -20,12 +18,11 @@ public class OffLine extends SequentialCommandGroup {
                 driveSubsystem.resetOdometry(trajectory1.getInitialPose());
                 driveSubsystem.zeroGyroscope();
             }),
-            new ShooterOn(shooterSubsystem, 0.435, true),
+            new InstantCommand(() -> shooterSubsystem.shooterOn(0.435)),
             new WaitCommand(2), // TODO: Test
-            new AugerOn(augerSubsystem, true),
+            new InstantCommand(() -> augerSubsystem.elevatorOn(true)),
             new WaitCommand(5), // TODO: Test
-            new AugerOn(augerSubsystem, false),
-            new ShooterOn(shooterSubsystem, 0.0, false),
+            new InstantCommand(() -> augerSubsystem.elevatorOff()),
             driveSubsystem.createCommandForTrajectory(trajectory1, false).withTimeout(15).withName("offLine")
         );
     }
