f1997c50dd865b615dc76c5fe76ad11b7d58d085
Jack_W() <willijoh005@usd232.org>
2022-03-15 23:17:40 -0500

    Flipped the signs the correct way

--- src/main/java/org/usd232/robotics/rapidreact/subsystems/HoodSubsystem.java
+++ src/main/java/org/usd232/robotics/rapidreact/subsystems/HoodSubsystem.java
@@ -68,6 +68,7 @@ public class HoodSubsystem extends SubsystemBase {
 
         double distance = hoodEncoder.getDistance();
         
+        // The signs are right: https://drive.google.com/file/d/18RI6TNe7JNofdJY-OnkNUPFjMOrOIRQu/view?usp=sharing
         if ((target - HoodConstants.HOOD_DEADBAND) <= distance 
                     || (target + HoodConstants.HOOD_DEADBAND) >= distance) { // If at + or - DEADBAND then dont move
             this.stopHood();
